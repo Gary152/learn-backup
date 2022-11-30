f2c26dc35dbbb6d9c667173fb90be60605fe1566
寂灭辰光 <2117135597@qq.com>
2022-11-30 09:05:43 +0800

    + 爬取收藏夹

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,10 +1,13 @@
 ﻿using HtmlAgilityPack;
 using Microsoft.Extensions.DependencyInjection;
+using System.Diagnostics;
 using System.IO;
 using System.IO.Compression;
+using System.Security.Claims;
 using System.Text;
 using System.Text.Json;
 using System.Text.RegularExpressions;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace ConsoleApp2
 {
@@ -12,15 +15,102 @@ namespace ConsoleApp2
     {
         public static void Main(string[] args)
         {
-            string str = "Is is the cost of of gasoline going up up";
-            var re = Regex.Match(str, @"\b(?<name>[a-zA-Z]+) \1");
-            while (re.Success)
-            {
+            TestGet();
 
-                Console.WriteLine(re.Value);
-                re=re.NextMatch();
+            Console.ReadKey();
+        }
+
+        public static async void TestGet()
+        {
+            var list = await GetFavorites();
+            Console.WriteLine("请选择：");
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                Console.WriteLine(i + 1 + ". " + list[i].title);
+            }
+
+            var index = int.Parse(Console.ReadLine() ?? "");
+
+            List<dynamic> videos = await GetVideos(list[index - 1].id);
+
+            var txt = "";
+            foreach (var item in videos)
+            {
+                txt += $"<dl><dt><a href=\"https://www.bilibili.com/video/{item.bvid}\">" +
+                    $"<img src=\"{item.cover}\" width=\"192\" height=\"108\" /></a></dt>" +
+                    $"<dd><a href=\"#\" onclick=\"F1('{item.bvid}')\">{item.title}" +
+                    $"</a></dd></dl>";
+            }
+            StreamReader sr = new StreamReader("test.html");
+            var model = sr.ReadToEnd();
+            sr.Close();
+            model = model.Replace("{{content}}", txt);
+            StreamWriter sw = new StreamWriter(list[index - 1].title + ".html");
+            sw.WriteLine(model);
+            sw.Close();
+        }
+
+        public static async Task<List<dynamic>> GetFavorites()
+        {
+            HttpClient httpClient = new();
+            httpClient.Timeout = new TimeSpan(0, 3, 0);
+            httpClient.DefaultRequestHeaders.Add("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36 Edg/107.0.1418.42");
+            httpClient.DefaultRequestHeaders.Add("cookie", "SESSDATA=59acb0c8%2C1685150710%2Cf3bb2%2Ab1");
+
+            var jsonResult = await httpClient.GetStringAsync("https://api.bilibili.com/x/v3/fav/folder/list4navigate");
+            var json = JsonDocument.Parse(jsonResult);
+            var list = json.RootElement.GetProperty("data")[0].GetProperty("mediaListResponse").GetProperty("list");
+
+            var items = new List<dynamic>();
+            for (int i = 0; i < list.GetArrayLength(); i++)
+            {
+                var title = list[i].GetProperty("title").ToString();
+                var id = list[i].GetProperty("id").ToString();
+                if (title == null || id == null) continue;
+                items.Add(new { title, id });
+            }
+
+            return items;
+        }
+
+        public static async Task<List<dynamic>> GetVideos(string id)
+        {
+            HttpClient httpClient = new();
+            httpClient.Timeout = new TimeSpan(0, 3, 0);
+            httpClient.DefaultRequestHeaders.Add("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36 Edg/107.0.1418.42");
+            httpClient.DefaultRequestHeaders.Add("cookie", "SESSDATA=59acb0c8%2C1685150710%2Cf3bb2%2Ab1");
+
+            var jsonResult = await httpClient.GetStringAsync($"https://api.bilibili.com/x/v3/fav/resource/list?media_id={id}&pn=1&ps=20&keyword=&order=mtime&type=0&tid=0&platform=web&jsonp=jsonp");
+            var json = JsonDocument.Parse(jsonResult);
+            var list = json.RootElement.GetProperty("data").GetProperty("medias");
+
+            var items = new List<dynamic>();
+            for (int i = 0; i < list.GetArrayLength(); i++)
+            {
+                var title = list[i].GetProperty("title").ToString();
+                var bvid = list[i].GetProperty("bvid").ToString();
+                var cover = list[i].GetProperty("cover").ToString();
+                if (title == null || id == null) continue;
+                items.Add(new { title, bvid, cover });
+            }
+
+            return items;
+        }
+
+        #region Regex
+        public void TestRegex()
+        {
+            string str = "Is is the cost of of gasoline going up up";
+            var res = Regex.Matches(str, @"\b(?<name>[a-zA-Z]+) \1");
+            foreach (Match item in res)
+            {
+                var a = item.Groups["name"];
+                Console.WriteLine(a);
             }
         }
+
+        #endregion
 
         #region Html
 
@@ -61,39 +151,6 @@ namespace ConsoleApp2
             Thread.Sleep(1000);//防止https认为是攻击
             var response = await client?.GetAsync(url)!;
             return await response.Content.ReadAsStringAsync();
-        }
-
-        #endregion
-
-        #region TimeSpan
-
-        private static void TimeSpan_start()
-        {
-            Regex regex = new Regex(@"^\d\d:\d\d:\d\d$");
-            Console.WriteLine("请输入TimeSpan");
-            string str = Console.ReadLine()!;
-            Match match = regex.Match(str);
-            double s = GetTimeSpanValue(match).TotalMilliseconds;
-            Console.WriteLine(s);
-        }
-        private static TimeSpan GetTimeSpanValue(Match match)
-        {
-            if (match.Success)
-            {
-                bool isTS = TimeSpan.TryParse(match.Value, out var outTS);
-                if (isTS)
-                {
-                    return outTS;
-                }
-                else
-                {
-                    return GetTimeSpanValue(match.NextMatch());
-                }
-            }
-            else
-            {
-                return TimeSpan.Zero;
-            }
         }
 
         #endregion
